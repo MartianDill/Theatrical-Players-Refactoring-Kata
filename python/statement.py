8fd8966f352a2fce337706b7b663808a61caf94b
Viktor <lazvik@ukr.net>
2023-06-10 19:56:24 +0300

    refactored py

--- python/statement.py
+++ python/statement.py
@@ -1,41 +1,42 @@
 import math
-
-
+# обчислення вартості виніс у окремий метод calculate_amount
+# прибрав метод format_as_dollars, тепер форматування безпосередньо при обчисленні result
+# замінив play['type'] на play_type для кращої читабельності
+# замінив perf['audience'] на audience для покращння читабельності та спрощення взаємодії
+# замінив this_amount на amount для спрощення та схожості з іншими змінними
 def statement(invoice, plays):
     total_amount = 0
     volume_credits = 0
     result = f'Statement for {invoice["customer"]}\n'
 
-    def format_as_dollars(amount):
-        return f"${amount:0,.2f}"
+    def calculate_amount(play_type, audience):
+        if play_type == "tragedy":
+            amount = 40000
+            if audience > 30:
+                amount += 1000 * (audience - 30)
+        elif play_type == "comedy":
+            amount = 30000
+            if audience > 20:
+                amount += 10000 + 500 * (audience - 20)
+            amount += 300 * audience
+        else:
+            raise ValueError(f'unknown type: {play_type}')
 
     for perf in invoice['performances']:
         play = plays[perf['playID']]
-        if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
-
-            this_amount += 300 * perf['audience']
-
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
+        play_type = play['type']
+        audience = perf['audience']
+        this_amount = calculate_amount(play_type, audience)
 
         # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
+        volume_credits += max(audience - 30, 0)
         # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
+        if play_type == "comedy":
+            volume_credits += math.floor(audience / 5)
         # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
+        result += f' {play["name"]}: ${this_amount/100:0,.2f} ({audience} seats)\n'
         total_amount += this_amount
 
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
+    result += f'Amount owed is ${total_amount/100:0,.2f}\n'
     result += f'You earned {volume_credits} credits\n'
     return result
-
-
